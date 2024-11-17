40119a0d9cc31defc322acfa18ab3116c1b90967
Mohamed Adnan Al Keblawy <alkeblawy.m@gmail.com>
2024-11-17 17:12:52 +0400

    Add Movie class , controler , and random view

---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Ajax.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Index()
+        {
+            return View();
+        }
+        // Get: Movies/Random
+        public ActionResult Random()
+        {
+            var movie = new Movie { Name = "Sherk!" };
+            return View(movie);
+        }
+    }
+}
