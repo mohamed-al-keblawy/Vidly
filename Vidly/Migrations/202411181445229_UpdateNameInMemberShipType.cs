4406b11a7f5d79dfa1c01e658cc87a68825fadaa
Mohamed Adnan Al Keblawy <alkeblawy.m@gmail.com>
2024-11-18 19:42:54 +0400

    Section 3 Exercise - Display membership type in the list of customers.

---
+++ Vidly/Migrations/202411181445229_UpdateNameInMemberShipType.cs
@@ -0,0 +1,20 @@
+﻿namespace Vidly.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class UpdateNameInMemberShipType : DbMigration
+    {
+        public override void Up()
+        {
+            Sql("Update MembershipTypes Set Name = 'Pay As You Go' Where Id = 1");
+            Sql("Update MembershipTypes Set Name = 'Weekly' Where Id = 2");
+            Sql("Update MembershipTypes Set Name = 'Monthly' Where Id = 3");
+            Sql("Update MembershipTypes Set Name = 'Yearly' Where Id = 4");
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
