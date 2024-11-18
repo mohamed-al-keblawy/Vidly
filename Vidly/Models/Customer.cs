fe7d966e64a09ddb1e731b063348a6ca6fe4d5ca
Mohamed Adnan Al Keblawy <alkeblawy.m@gmail.com>
2024-11-18 21:25:06 +0400

    Section 3 Exercise - Display customer's birthdate on their profile page.

--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -12,6 +12,8 @@ namespace Vidly.Models
         [Required]
         [StringLength(255)]
         public string Name { get; set; }
+
+        public DateTime? BirthDate { get; set; }
         public bool IsSubscribedToNewsLetter { get; set; }
         public MembershipType MembershipType { get; set; }
         public byte MembershipTypeId { get; set; }
