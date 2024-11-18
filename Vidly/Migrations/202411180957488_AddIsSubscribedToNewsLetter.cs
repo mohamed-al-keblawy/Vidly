a812327b14033ad2e0de2734ace59f2a880cfcf4
Mohamed Adnan Al Keblawy <alkeblawy.m@gmail.com>
2024-11-18 14:26:12 +0400

    MigrateDB and Seed MembershipTypes

---
+++ Vidly/Migrations/202411180957488_AddIsSubscribedToNewsLetter.cs
@@ -0,0 +1,18 @@
+﻿namespace Vidly.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddIsSubscribedToNewsLetter : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Customers", "IsSubscribedToNewsLetter", c => c.Boolean(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Customers", "IsSubscribedToNewsLetter");
+        }
+    }
+}
