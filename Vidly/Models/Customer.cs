8ee4fa68df5517cbbfacf7a4550f3fcc2fb31256
Mohamed Adnan Al Keblawy <alkeblawy.m@gmail.com>
2025-01-04 23:20:37 +0400

    Custom Validation for Min 18 years age

--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -10,11 +10,12 @@ namespace Vidly.Models
     {
         public int Id { get; set; }
        
-        [Required]
+        [Required(ErrorMessage ="Please enter customer's name.")]
         [StringLength(255)]
         public string Name { get; set; }
 
         [Display(Name="Date of Birth")]
+        [Min18YearsIfAMember]
         public DateTime? BirthDate { get; set; }
         public bool IsSubscribedToNewsLetter { get; set; }
         public MembershipType MembershipType { get; set; }
