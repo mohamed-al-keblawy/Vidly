7a1ebb3a5cb1d8bd490591785f00c774a6cb126e
Mohamed Adnan Al Keblawy <alkeblawy.m@gmail.com>
2025-01-04 22:38:25 +0400

    Add Validation styles in save customer

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -49,6 +49,16 @@ namespace Vidly.Controllers
         [HttpPost]
         public ActionResult Save(Customer customer) // could use UpdateCustomerDto Data transfer object
         {
+            if (!ModelState.IsValid)
+            {
+                var viewModel = new CustomerFormViewModel
+                {
+                    Customer = customer,
+                    MemmbershipTypes = _context.MembershipTypes.ToList()
+                };
+
+                return View("CustomerForm", viewModel);
+            }
             if (customer.Id == 0)
                 _context.Customers.Add(customer);
             else 
